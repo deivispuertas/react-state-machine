68ff60b781136151d329476ef53dea65a2d012a3
devpuertas <deivispuertas0@gmail.com>
2025-05-07 11:36:49 -0500

    feat: enhance state machine with configuration and wizard steps

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,48 @@ type WizardState = {
 
 type StepNames = "step1" | "step2" | "confirmation";
 
+const stateMachineConfig: StateMachineConfig<WizardState, StepNames> = {
+  initialStep: "step1",
+  steps: {
+    step1: {
+      canAdvance: (state) => !!state.name // convertir en booleano !!
+    },
+    step2: {
+      canAdvance: (state) => !!state.name // convertir en booleano !!
+    },
+    confirmation: {
+      canAdvance: () => true // convertir en booleano !!
+    }
+  },
+  views: {
+    step1: ({ state, setState }) => (
+      <div>
+        <input 
+          type="text" 
+          value={state.name}
+          onChange={(e) => setState((prev) => ({...prev, name: e.target.value})) }
+          placeholder='Full name'
+        />
+      </div>
+    ),
+    step2: ({ state, setState }) => (
+      <div>
+        <input 
+          type="numer" 
+          value={state.age}
+          onChange={(e) => setState((prev) => ({...prev, age: parseInt(e.target.value)})) }
+          placeholder='Full name'
+        />
+      </div>
+    ),
+    confirmation: ({ state }) => (
+      <div>
+        <p>{state.name} is {state.age} years old.</p>
+      </div>
+    )
+  }
+}
+
 function App() {
 
   return (
